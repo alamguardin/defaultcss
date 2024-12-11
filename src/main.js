fca62d9eb84cee7b56280dcbe739ec8abb6767a1
Alam Guardin <alamguardin@gmail.com>
2024-12-10 22:22:04 -0500

    style: add styles and interaction to modal element

--- src/main.js
+++ src/main.js
@@ -3,3 +3,16 @@ import "./output/default.css";
 document.addEventListener("submit", (e) => {
 	e.preventDefault();
 });
+
+// Modal example
+const modal = document.querySelector("#modal");
+const openModal = document.querySelector("#showModal");
+const closeModal = document.querySelector("#cancel");
+
+openModal.addEventListener("click", () => {
+	modal.showModal();
+});
+
+closeModal.addEventListener("click", () => {
+	modal.close();
+});
